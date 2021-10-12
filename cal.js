6cfe188e1790f36eaf883f7b0b1af7318a4300a2
ankitkumar0622 <92391587+ankitkumar0622@users.noreply.github.com>
2021-10-12 22:24:11 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,44 @@
+let screen = document.getElementById('screen');
+buttons = document.querySelectorAll('button');
+let screenValue = '';
+let h = '';
+for (item of buttons) {
+    item.addEventListener('click', (e) => {
+        buttonText = e.target.innerText;
+        console.log('Button text is ', buttonText);
+        if (buttonText == 'X') {
+            buttonText = '*';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else if (buttonText == 'C') {
+            screenValue = "";
+            screen.value = screenValue;
+        }
+        else if (buttonText == 'del') {
+            screen.value = screen.value.slice(0, -1);
+            screenValue = screen.value;
+        }
+        else if (buttonText == '=') {
+            h = screen.value + '=' + eval(screenValue);
+            screen.value = eval(screenValue);
+        }
+        else if (buttonText == '√ ') {
+            screenValue = Math.sqrt(screenValue);
+            screen.value = screenValue;
+        }
+        else if (buttonText == 'hty') {
+            screen.value = h;
+        }
+        else {
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+
+
+
+
+
+
+    })
+}
